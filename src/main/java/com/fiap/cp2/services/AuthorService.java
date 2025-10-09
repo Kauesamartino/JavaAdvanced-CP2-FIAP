baff6c59d8d0fc701477f5e2d663403b01631ad9
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-09 02:58:02 -0300

    feat: create AuthorService interface for author retrieval with pagination

---
+++ src/main/java/com/fiap/cp2/services/AuthorService.java
@@ -0,0 +1,9 @@
+package com.fiap.cp2.services;
+
+import com.fiap.cp2.entities.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface AuthorService {
+    Page<Author> findAll(Pageable pageable);
+}
---
+++ src/main/java/com/fiap/cp2/services/AuthorServiceImpl.java
@@ -0,0 +1,22 @@
+package com.fiap.cp2.services;
+
+import com.fiap.cp2.entities.Author;
+import com.fiap.cp2.repositories.AuthorRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public Page<Author> findAll(Pageable pageable) {
+        return this.authorRepository.findAll(pageable);
+    }
+}
