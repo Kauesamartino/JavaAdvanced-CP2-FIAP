390452c8323118c5aff2151d416476226ac4d96c
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-09 03:45:32 -0300

    feat: add CRUD endpoints for Book in BookController

--- src/main/java/com/fiap/cp2/controllers/BookController.java
+++ src/main/java/com/fiap/cp2/controllers/BookController.java
@@ -1,5 +1,6 @@
 package com.fiap.cp2.controllers;
 
+import com.fiap.cp2.dto.BookInDto;
 import com.fiap.cp2.dto.BookOutDto;
 import com.fiap.cp2.entities.Book;
 import com.fiap.cp2.mappers.BookMapper;
@@ -7,11 +8,9 @@ import com.fiap.cp2.services.BookService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +34,28 @@ public class BookController {
         return ResponseEntity.ok(new PageImpl<>(dto, books.getPageable(), books.getTotalElements()));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<BookOutDto> findById(@PathVariable Long id) {
+        final Book book = this.bookService.findById(id);
+        return ResponseEntity.ok(BookMapper.toOutDto(book));
+    }
+
+    @PostMapping
+    public ResponseEntity<BookOutDto> createBook(@RequestBody BookInDto book) {
+        final Book savedBook = this.bookService.saveOrUpdate(BookMapper.toEntity(book));
+        return ResponseEntity.ok(BookMapper.toOutDto(savedBook));
+    }
+
+    @PutMapping
+    public ResponseEntity<BookOutDto> createBook(@RequestBody BookInDto book) {
+        final Book savedBook = this.bookService.saveOrUpdate(BookMapper.toEntity(book));
+        return ResponseEntity.ok(BookMapper.toOutDto(savedBook));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        this.bookService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
 }
